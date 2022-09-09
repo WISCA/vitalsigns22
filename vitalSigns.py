4bc671145f07c11e59cf5d833f01b68916fd3b6d
Jacob Holtom <jacob@holtom.me>
2022-09-09 14:26:03 -0700

    Fixed up a bunch of stuff

--- vitalSigns.py
+++ vitalSigns.py
@@ -28,7 +28,6 @@ from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import fft
 from gnuradio.fft import window
-from gnuradio import filter
 from gnuradio import gr
 import sys
 import signal
@@ -81,20 +80,22 @@ class vitalSigns(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.window_size = window_size = 128
-        self.tx_gain = tx_gain = 50
-        self.samp_rate = samp_rate = 1e6
-        self.rx_gain = rx_gain = 50
-        self.freq = freq = 4e9
-        self.decimRate = decimRate = 12
+        self.window_size = window_size = 2048
+        self.tx_gain = tx_gain = 55
+        self.samp_rate = samp_rate = 500e3
+        self.rx_gain = rx_gain = 55
+        self.freq = freq = 5.5e9
+        self.display_buffer = display_buffer = 5000
+        self.decimRate = decimRate = 1
+        self.amplitude = amplitude = 0.9
 
         ##################################################
         # Blocks
         ##################################################
-        self._tx_gain_range = Range(0, 100, 1, 50, 200)
+        self._tx_gain_range = Range(0, 100, 1, 55, 200)
         self._tx_gain_win = RangeWidget(self._tx_gain_range, self.set_tx_gain, "'tx_gain'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._tx_gain_win)
-        self._rx_gain_range = Range(0, 100, 1, 50, 200)
+        self._rx_gain_range = Range(0, 100, 1, 55, 200)
         self._rx_gain_win = RangeWidget(self._rx_gain_range, self.set_rx_gain, "'rx_gain'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._rx_gain_win)
         self.uhd_usrp_source_0 = uhd.usrp_source(
@@ -129,23 +130,23 @@ class vitalSigns(gr.top_block, Qt.QWidget):
         self.uhd_usrp_sink_0.set_bandwidth(samp_rate, 0)
         self.uhd_usrp_sink_0.set_gain(tx_gain, 0)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-            window_size, #size
-            samp_rate/decimRate/window_size, #samp_rate
+            display_buffer, #size
+            samp_rate/window_size/decimRate, #samp_rate
             "", #name
             1, #number of inputs
             None # parent
         )
         self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        self.qtgui_time_sink_x_0.set_y_axis(-180, 180)
 
         self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
 
         self.qtgui_time_sink_x_0.enable_tags(True)
         self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_autoscale(True)
         self.qtgui_time_sink_x_0.enable_grid(False)
         self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_control_panel(True)
         self.qtgui_time_sink_x_0.enable_stem_plot(False)
 
 
@@ -176,33 +177,19 @@ class vitalSigns(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.low_pass_filter_0 = filter.fir_filter_fff(
-            decimRate,
-            firdes.low_pass(
-                1,
-                samp_rate/window_size,
-                300,
-                100,
-                window.WIN_HAMMING,
-                6.76))
-        self.fft_vxx_0 = fft.fft_vcc(window_size, True, window.blackmanharris(window_size), True, 1)
-        self.epy_block_0 = epy_block_0.blk()
-        self.dc_blocker_xx_0 = filter.dc_blocker_ff(32, True)
+        self.fft_vxx_0 = fft.fft_vcc(window_size, True, [], False, 1)
+        self.epy_block_0 = epy_block_0.blk(vecSize=window_size)
         self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, window_size)
-        self.blocks_delay_0 = blocks.delay(gr.sizeof_float*1, window_size-1)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_CONST_WAVE, 1, 1, 0, 0)
+        self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1e6/window_size, amplitude, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.uhd_usrp_sink_0, 0))
-        self.connect((self.blocks_delay_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.uhd_usrp_sink_0, 0))
         self.connect((self.blocks_stream_to_vector_0, 0), (self.fft_vxx_0, 0))
-        self.connect((self.dc_blocker_xx_0, 0), (self.low_pass_filter_0, 0))
-        self.connect((self.epy_block_0, 0), (self.dc_blocker_xx_0, 0))
+        self.connect((self.epy_block_0, 0), (self.qtgui_time_sink_x_0, 0))
         self.connect((self.fft_vxx_0, 0), (self.epy_block_0, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.blocks_delay_0, 0))
         self.connect((self.uhd_usrp_source_0, 0), (self.blocks_stream_to_vector_0, 0))
 
 
@@ -219,9 +206,8 @@ class vitalSigns(gr.top_block, Qt.QWidget):
 
     def set_window_size(self, window_size):
         self.window_size = window_size
-        self.blocks_delay_0.set_dly(self.window_size-1)
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate/self.window_size, 300, 100, window.WIN_HAMMING, 6.76))
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.decimRate/self.window_size)
+        self.analog_sig_source_x_0_0.set_frequency(1e6/self.window_size)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.window_size/self.decimRate)
 
     def get_tx_gain(self):
         return self.tx_gain
@@ -235,9 +221,8 @@ class vitalSigns(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate/self.window_size, 300, 100, window.WIN_HAMMING, 6.76))
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.decimRate/self.window_size)
+        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.window_size/self.decimRate)
         self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
         self.uhd_usrp_sink_0.set_bandwidth(self.samp_rate, 0)
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
@@ -258,12 +243,25 @@ class vitalSigns(gr.top_block, Qt.QWidget):
         self.uhd_usrp_sink_0.set_center_freq(self.freq, 0)
         self.uhd_usrp_source_0.set_center_freq(self.freq, 0)
 
+    def get_display_buffer(self):
+        return self.display_buffer
+
+    def set_display_buffer(self, display_buffer):
+        self.display_buffer = display_buffer
+
     def get_decimRate(self):
         return self.decimRate
 
     def set_decimRate(self, decimRate):
         self.decimRate = decimRate
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.decimRate/self.window_size)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.window_size/self.decimRate)
+
+    def get_amplitude(self):
+        return self.amplitude
+
+    def set_amplitude(self, amplitude):
+        self.amplitude = amplitude
+        self.analog_sig_source_x_0_0.set_amplitude(self.amplitude)
 
 
 
--- vitalSigns_epy_block_0.py
+++ vitalSigns_epy_block_0.py
@@ -13,16 +13,18 @@ from gnuradio import gr
 class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
     """Embedded Python Block example - a simple multiply const"""
 
-    def __init__(self):  # only default arguments here
+    def __init__(self, vecSize=2048):  # only default arguments here
         """arguments to this function show up as parameters in GRC"""
         gr.sync_block.__init__(
             self,
             name='Vector Max',   # will show up in GRC
-            in_sig=[(np.complex64, 128)],
+            in_sig=[(np.complex64, vecSize)],
             out_sig=[np.float32]
         )
 
     def work(self, input_items, output_items):
         """example: multiply with constant"""
-        output_items[0][0] = np.angle(np.max(input_items[0][0]))
-        return 1
+        for vectorIndex in range(len(input_items[0])):
+            maxValue = (180/np.pi)*np.angle(np.max(input_items[0][vectorIndex]))
+            output_items[0][vectorIndex] = maxValue
+        return len(output_items[0])
