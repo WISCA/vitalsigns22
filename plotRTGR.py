92ed343f896bf546481b226e447f5626854047e0
Jacob Holtom <jacob@holtom.me>
2022-09-19 13:10:38 -0700

    some updates on the blocked processing

--- plotRTGR.py
+++ plotRTGR.py
@@ -10,11 +10,12 @@ samp_rate = 500e3
 window_size = 100
 data_rate = samp_rate/window_size
 plot_time = 10
+avg_interval = 15 # Processing Interval (Seconds)
 num_plot_samps = int(np.ceil(5*data_rate))
-data_samps = np.zeros(num_plot_samps)
+num_est_samps = int(np.ceil(avg_interval*data_rate))
+data_samps = np.zeros(num_est_samps)
 plot_samps = np.zeros(num_plot_samps)
 plot_range = np.arange(0,num_plot_samps)/data_rate
-avg_interval = 15 # Processing Interval (Seconds)
 
 # Setup ZMQ
 context = zmq.Context()
@@ -40,10 +41,29 @@ def zmq_handler():
         data_samps = np.concatenate((data_samps, data),axis=None)
         plot_samps = np.unwrap(data_samps[-num_plot_samps:])
 
+
+def estimateVitals():
+    threading.Timer(avg_interval, estimateVitals).start()
+    # DC Trend Removal
+    est_samps = np.unwrap(data_samps[-num_est_samps:])
+    x = np.arange(0,num_est_samps)/data_rate
+    offset = np.polyfit(x, est_samps, 5)
+    dcpoly = np.poly1d(offset)
+    dcoffset = dcpoly(x)
+    dcrem = est_samps - dcoffset
+    # FFT For RR
+    freq = np.fft.rfft(dcrem)
+    f = np.fft.rfftfreq(num_est_samps, 1/data_rate)
+    rrmax = np.argmax(freq)
+    print(f[rrmax])
+
+
 if __name__ == '__main__':
     thread = threading.Thread(target=zmq_handler)
     thread.daemon = True
     thread.start()
+
+    estimateVitals()
 
     fig = plt.figure()
     line, = plt.plot(plot_range, plot_samps)
