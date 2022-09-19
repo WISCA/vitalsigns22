f9faad163cfee1d67cb63f0b9e7eeac0c0f38c0b
Jacob Holtom <jacob@holtom.me>
2022-09-19 07:54:02 -0700

    add traditional CW processing

--- vitalSigns.py
+++ vitalSigns.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Not titled yet
+# Title: Vital Signs GRCon 2022 Demo
 # GNU Radio version: 3.10.1.0
 
 from packaging.version import Version as StrictVersion
@@ -26,9 +26,9 @@ from gnuradio.filter import firdes
 import sip
 from gnuradio import analog
 from gnuradio import blocks
-from gnuradio import fft
-from gnuradio.fft import window
+from gnuradio import filter
 from gnuradio import gr
+from gnuradio.fft import window
 import sys
 import signal
 from argparse import ArgumentParser
@@ -36,10 +36,8 @@ from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import uhd
 import time
-from gnuradio import zeromq
 from gnuradio.qtgui import Range, RangeWidget
 from PyQt5 import QtCore
-import vitalSigns_epy_block_0 as epy_block_0  # embedded python block
 
 
 
@@ -48,9 +46,9 @@ from gnuradio import qtgui
 class vitalSigns(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        gr.top_block.__init__(self, "Vital Signs GRCon 2022 Demo", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Not titled yet")
+        self.setWindowTitle("Vital Signs GRCon 2022 Demo")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -87,7 +85,7 @@ class vitalSigns(gr.top_block, Qt.QWidget):
         self.rx_gain = rx_gain = 55
         self.freq = freq = 4e9
         self.display_buffer = display_buffer = 5000
-        self.decimRate = decimRate = 1
+        self.decimRate = decimRate = 100
         self.amplitude = amplitude = 0.9
 
         ##################################################
@@ -99,7 +97,6 @@ class vitalSigns(gr.top_block, Qt.QWidget):
         self._rx_gain_range = Range(0, 100, 1, 55, 200)
         self._rx_gain_win = RangeWidget(self._rx_gain_range, self.set_rx_gain, "'rx_gain'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._rx_gain_win)
-        self.zeromq_push_sink_0 = zeromq.push_sink(gr.sizeof_float, 1, 'tcp://127.0.0.1:5557', 100, False, -1)
         self.uhd_usrp_source_0 = uhd.usrp_source(
             ",".join(("", '')),
             uhd.stream_args(
@@ -133,7 +130,7 @@ class vitalSigns(gr.top_block, Qt.QWidget):
         self.uhd_usrp_sink_0.set_gain(tx_gain, 0)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
             display_buffer, #size
-            samp_rate/window_size/decimRate, #samp_rate
+            samp_rate/decimRate, #samp_rate
             "", #name
             1, #number of inputs
             None # parent
@@ -179,21 +176,73 @@ class vitalSigns(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.fft_vxx_0 = fft.fft_vcc(window_size, True, [], False, 1)
-        self.epy_block_0 = epy_block_0.blk(vecSize=window_size)
-        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, window_size)
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
+            1024, #size
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate/decimRate, #bw
+            "", #name
+            1,
+            None # parent
+        )
+        self.qtgui_freq_sink_x_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_0.enable_control_panel(False)
+        self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
+
+
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.blocks_multiply_conjugate_cc_0 = blocks.multiply_conjugate_cc(1)
+        self.blocks_complex_to_arg_0 = blocks.complex_to_arg(1)
+        self.band_pass_filter_0 = filter.fir_filter_ccf(
+            decimRate,
+            firdes.band_pass(
+                1,
+                samp_rate,
+                1,
+                500,
+                10,
+                window.WIN_HAMMING,
+                6.76))
         self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1e6/window_size, amplitude, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_conjugate_cc_0, 0))
         self.connect((self.analog_sig_source_x_0_0, 0), (self.uhd_usrp_sink_0, 0))
-        self.connect((self.blocks_stream_to_vector_0, 0), (self.fft_vxx_0, 0))
-        self.connect((self.epy_block_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.epy_block_0, 0), (self.zeromq_push_sink_0, 0))
-        self.connect((self.fft_vxx_0, 0), (self.epy_block_0, 0))
-        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.band_pass_filter_0, 0), (self.blocks_complex_to_arg_0, 0))
+        self.connect((self.band_pass_filter_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_complex_to_arg_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_multiply_conjugate_cc_0, 0), (self.band_pass_filter_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_multiply_conjugate_cc_0, 1))
 
 
     def closeEvent(self, event):
@@ -210,7 +259,6 @@ class vitalSigns(gr.top_block, Qt.QWidget):
     def set_window_size(self, window_size):
         self.window_size = window_size
         self.analog_sig_source_x_0_0.set_frequency(1e6/self.window_size)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.window_size/self.decimRate)
 
     def get_tx_gain(self):
         return self.tx_gain
@@ -225,7 +273,9 @@ class vitalSigns(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.window_size/self.decimRate)
+        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, 1, 500, 10, window.WIN_HAMMING, 6.76))
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate/self.decimRate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.decimRate)
         self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
         self.uhd_usrp_sink_0.set_bandwidth(self.samp_rate, 0)
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
@@ -257,7 +307,8 @@ class vitalSigns(gr.top_block, Qt.QWidget):
 
     def set_decimRate(self, decimRate):
         self.decimRate = decimRate
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.window_size/self.decimRate)
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate/self.decimRate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.decimRate)
 
     def get_amplitude(self):
         return self.amplitude
